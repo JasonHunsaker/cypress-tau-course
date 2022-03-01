c1adcd6165fb0c74ef1b9dbf96f0d4ee33cd372e
Jason Hunsaker <jasonmhunsaker@gmail.com>
2022-02-28 21:24:21 -0500

    Update multiple_assertions_start.js

--- cypress/integration/multiple_assertions_start.js
+++ cypress/integration/multiple_assertions_start.js
@@ -3,19 +3,19 @@
 beforeEach(() => {
 
   cy
-    .visit('/board/2305140181')
+    .visit('/board/42257192965')
 })
 
 it('Multiple assertions', () => {
 
   cy
-    .get('[data-cy=task]')
-    .eq(0)
-    .should('contain.text', 'bread')
-
-  cy
-    .get('[data-cy=task]')
-    .eq(1)
-    .should('contain.text', 'milk')
+    .get('[data-cy=task]')   //then does not contain retry logic - no 4 second "wait" period - would need to be a .should instead
+    .then(item => {
+      if (item.length !== 3) {
+        throw new Error('Not enough elements!')
+      }                              
+      expect(item[0]).to.contain.text('tasks')
+      expect(item[1]).to.contain.text('Milk')
+    })
 
 })
